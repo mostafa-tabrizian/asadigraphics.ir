a48bef4198e6b1346af8d771b89c5c270435345c
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-10-08 19:09:00 +0330

    *searchInput icon absolute and on click submit the form

--- src/components/searchInput.tsx
+++ src/components/searchInput.tsx
@@ -41,16 +41,16 @@ const SearchInput = () => {
       <div className='flex justify-center md:w-auto items-center relative mt-2'>
          <form
             onSubmit={handleFormSubmit}
-            className='border grid grid-cols-6 justify-between border-slate-300 rounded-lg bg-slate-100 w-full '
+            className='border relative justify-between border-slate-300 rounded-lg bg-slate-100 w-full '
          >
             <input
                onChange={(e) => setSearchQuery(e.target.value)}
-               className='text-sm rtl py-2 col-span-5 outline-none yekanBold text-center placeholder:text-center text-slate-800 placeholder:text-slate-500'
+               className='text-sm rtl w-full py-2 outline-none yekanBold text-center placeholder:text-center text-slate-800 placeholder:text-slate-500'
                type='text'
                placeholder='جستجو میان طرح ها ...'
             />
             <svg
-               className='h-7 w-7 m-auto text-orange-400'
+               className='h-7 w-7 m-auto absolute right-5 top-1 text-orange-400 hover:cursor-pointer'
                width='24'
                height='24'
                viewBox='0 0 24 24'
@@ -59,6 +59,7 @@ const SearchInput = () => {
                fill='none'
                strokeLinecap='round'
                strokeLinejoin='round'
+               onClick={handleFormSubmit}
             >
                {' '}
                <path stroke='none' d='M0 0h24v24H0z' /> <circle cx='10' cy='10' r='7' />{' '}
